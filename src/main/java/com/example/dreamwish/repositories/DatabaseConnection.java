6337d685d8adb3da98dd608c4c5d7d59086ef132
Søs <slys87@gmail.com>
2022-05-04 08:48:52 +0200

    Pushypushy

--- src/main/java/com/example/dreamwish/repositories/DatabaseConnection.java
+++ src/main/java/com/example/dreamwish/repositories/DatabaseConnection.java
@@ -15,7 +15,6 @@ public class DatabaseConnection {
             /**String url ="jdbc:mysql://dreamwish.mysql.database.azure.com:3306/dream_wish?useSSL=true&requireSSL=false"; myDbConn = DriverManager.getConnection(url, "teamcharlie@dreamwish", {your_password});**/
             String url ="jdbc:mysql://dreamwish.mysql.database.azure.com:3306/dream_wish?useSSL=true&requireSSL=false";
             Connection con = DriverManager.getConnection(url, "teamcharlie@dreamwish", "Kea22charlie");
-            //Connection con = DriverManager.getConnection(url,"root","Kiy3ia3#");
             return con;
         } catch (Exception e) {
             return null;
